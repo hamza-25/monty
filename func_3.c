0f5874263449a12e39faa0e0b9db4688a020accc
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-25 18:09:10 +0100

    Update func_3.c

--- func_3.c
+++ func_3.c
@@ -7,27 +7,27 @@
 */
 void f_div(stack_t **stack, unsigned int line_number)
 {
-        stack_t *current = *stack, *temp = *stack;
-        int count = 0, keep;
+	stack_t *current = *stack, *temp = *stack;
+	int count = 0, keep;
 
+	while (current)
+	{
+		current = current->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		free_stack(global_data.top), fclose(global_data.file), exit(1);
+	}
 	if (current->n == 0)
-        {
+	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
 		free_stack(global_data.top), fclose(global_data.file), exit(1);
 	}
-	while (current)
-	{
-                current = current->next;
-                count++;
-        }
-        if (count < 2)
-        {
-                fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
-                free_stack(global_data.top), fclose(global_data.file), exit(1);
-        }
 	keep = (*stack)->n;
-        *stack = (*stack)->next;
-        (*stack)->n = ((*stack)->n) / keep;
-        global_data.top = *stack;
-        free(temp);
+	*stack = (*stack)->next;
+	(*stack)->n = ((*stack)->n) / keep;
+	global_data.top = *stack;
+	free(temp);
 }
